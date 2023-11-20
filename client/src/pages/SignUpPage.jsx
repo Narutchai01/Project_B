535bbd8ed41a36dd5f0001b5074c7d1bedcad7c8
FC4RICA <kantinan.fw@gmail.com>
2023-11-20 07:10:43 +0700

    homepage

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 import axios from "axios"
+import "./SignUpPage.css"
 
-
-const SignUpPage = ({ setSignUpPage, setLoginPage }) => {
+const SignUpPage = ({ setSignUpPage, setLoginPage, setCubeFocus }) => {
 
   const [SignUp, setSignUp] = useState({
     username: ''
@@ -21,23 +21,28 @@ const SignUpPage = ({ setSignUpPage, setLoginPage }) => {
       , email: SignUp.email
       , password: SignUp.password
     }
-    axios.post('http://localhost:8080/api/register', createNewUser)
+    axios.post("http://localhost:8080/api/register", createNewUser)
+
     console.log(SignUp)
+    
     setSignUpPage(false)
     setLoginPage(true)
+    setCubeFocus("login")
   }
 
   return (
     <>
-      <h1 className="">SignUp</h1>
-      <form className="">
-        <input type="email" name="email" placeholder="Email" className="" onChange={handleChange} />
-        <input type="text" name="username" placeholder="Username" className="" onChange={handleChange} />
-        <input type="password" name="password" placeholder="Password" className="" onChange={handleChange} />
-        <button type="submit" className="" onClick={handelSignUp}>
-            Create Account
-        </button>
-      </form>
+      <div className="sign-up-page">
+        <h1 className="">SignUp</h1>
+        <form className="">
+          <input type="email" name="email" placeholder="Email" className="" onChange={handleChange} />
+          <input type="text" name="username" placeholder="Username" className="" onChange={handleChange} />
+          <input type="password" name="password" placeholder="Password" className="" onChange={handleChange} />
+          <button type="submit" className="" onClick={handelSignUp}>
+              Create Account
+          </button>
+        </form>
+      </div>
     </>
   )}
 
